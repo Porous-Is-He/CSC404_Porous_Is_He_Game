e79e1d04207e61058de3443e3afdf3cb305cbb8a
Jenny Ho <jenny.ho@mail.utoronto.ca>
2023-10-01 12:59:32 -0400

    scale down water as it gets absorbed

--- Porous Is He/Assets/Scripts/LiquidSource.cs
+++ Porous Is He/Assets/Scripts/LiquidSource.cs
@@ -8,34 +8,46 @@ public class LiquidSource : MonoBehaviour
 
     public string liquidType;
     public int maxLiquid;
+
     private LiquidInfo liquid;
     private const int INFINITE = -1;
+
+    private Vector3 originalScale;
 
 
 
     void Start()
     {
         liquid = new LiquidInfo(liquidType, maxLiquid);
+        originalScale = transform.localScale;
+     
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M)) {
+            
+            AbsorbLiquid(2);
+
+        }
     }
 
     public LiquidInfo AbsorbLiquid(int amount)
     {
         if (maxLiquid == INFINITE) return liquid;
 
-        liquid.liquidAmount -= amount;
-
 
         if (liquid.liquidAmount - amount <= 0)
         {
             liquid.liquidAmount = 0;
+            Destroy(gameObject);
         } else
         {
             liquid.liquidAmount -= amount;
+            float liquidRatio = (float)liquid.liquidAmount/(float)maxLiquid;
+            Vector3 newScale = new Vector3(originalScale.x * liquidRatio, originalScale.y * liquidRatio, originalScale.z * liquidRatio);
+            transform.localScale = newScale;
+
         }
 
         return liquid;
