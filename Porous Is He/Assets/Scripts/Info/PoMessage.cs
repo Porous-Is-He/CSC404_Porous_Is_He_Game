6a3b346482ed668830e7cfa12d736d30ebbaca9e
nzhou27 <zhouhanzhi0818@gmail.com>
2023-10-16 22:16:25 -0400

    42 dialogue UI and script finnished

---
+++ Porous Is He/Assets/Scripts/Info/PoMessage.cs
@@ -0,0 +1,13 @@
+public class PoMessage
+{
+    public string message;
+
+    public float time;
+
+    public PoMessage(string message, float time)
+    {
+        this.message = message;
+        this.time = time;
+    }
+
+}
