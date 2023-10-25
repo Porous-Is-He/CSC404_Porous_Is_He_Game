d8f8c7efa23c1d8b22ddabfede44a5c40280e598
nzhou27 <zhouhanzhi0818@gmail.com>
2023-10-25 18:51:55 -0400

    add headbutt feature

--- Porous Is He/Assets/Scripts/HeavyCollider.cs
+++ Porous Is He/Assets/Scripts/HeavyCollider.cs
@@ -8,40 +8,48 @@ public class HeavyCollider : MonoBehaviour
     public GameObject brokenObject;
     public GameObject currentObject;
     public bool isBroken = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        
 
     }
 
 
-     void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
-        if (isBroken == true)
+
+        if (Input.GetKey(KeyCode.I))
         {
-            return;
-        }
-
-
-        if (other.gameObject.tag == "Player")
-        {
-            int playerWeight = GameObject.Find("Player").GetComponent<LiquidTracker>().CalcWeight();
-
-            if (playerWeight >= GameObject.Find("Player").GetComponent<LiquidTracker>().maxLiquidAmount)
+            Debug.Log("presses key");
+            if (isBroken == true)
             {
-                //GameObject broken = Instantiate(brokenObject, currentObject.transform.position, currentObject.transform.rotation, currentObject.transform.parent);
-                //broken.transform.localScale = currentObject.transform.localScale;
-                brokenObject.SetActive(true);
-                currentObject.SetActive(false);
-                isBroken = true;
+                return;
+            }
+
+
+            if (other.gameObject.tag == "Player")
+            {
+                int playerWeight = GameObject.Find("Player").GetComponent<LiquidTracker>().CalcWeight();
+
+                if (playerWeight >= GameObject.Find("Player").GetComponent<LiquidTracker>().maxLiquidAmount)
+                {
+                    //GameObject broken = Instantiate(brokenObject, currentObject.transform.position, currentObject.transform.rotation, currentObject.transform.parent);
+                    //broken.transform.localScale = currentObject.transform.localScale;
+                    brokenObject.SetActive(true);
+                    currentObject.SetActive(false);
+                    isBroken = true;
+                }
             }
         }
+
     }
 }
