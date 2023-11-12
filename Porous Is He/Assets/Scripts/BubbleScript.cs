cbf24911a8c5b460b70cc7948f71cf6195bf5392
DAVIDMA0808 <jianxiang.ma@mail.utoronto.ca>
2023-11-12 18:49:40 -0500

    Attempted to add floating animation

--- Porous Is He/Assets/Scripts/BubbleScript.cs
+++ Porous Is He/Assets/Scripts/BubbleScript.cs
@@ -2,22 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script is in charge of the "Bubbles"
+// The main thing is that it has a floating animation
+// As well as disappearing when collided by the player
 public class BubbleScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    public float amplitude = 0.1f;
+    public float frequency = 1f;
+    private Vector3 posOffset = new Vector3 ();
+    private Vector3 tempPosition = new Vector3 ();
+
     public void Start()
     {
         GetComponent<Renderer>().enabled = true;
+        tempPosition = transform.position;
+    }
+
+    public void Update()
+    {
+
+        // Float up/down with a Sin()
+        tempPosition = posOffset;
+        tempPosition.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPosition;
     }
 
     public void OnTriggerEnter(Collider other) {
         if (other.transform.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Collision Detected.");
+
             GetComponent<Renderer>().enabled = false;
         }
 
-        Debug.Log("Bubble should be invisible here.");
     }
 }
