804ac8a2b061cf67632cf37d9cfcbb14f3829abd
DAVIDMA0808 <jianxiang.ma@mail.utoronto.ca>
2023-12-01 18:36:25 -0500

    bubble counter works for level 1

--- Porous Is He/Assets/Scripts/BubbleCountingScript.cs
+++ Porous Is He/Assets/Scripts/BubbleCountingScript.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BubbleCountingScript : MonoBehaviour
 {
 
-    private int coin = 0;
+    private int bubbles = 0;
+    public TextMeshProUGUI bubbleText;
 
     public void OnTriggerEnter(Collider other) {
         if (other.transform.tag == "Bubble")
         {
             other.gameObject.GetComponent<AudioSource>().Play();
-            coin++;
-            Debug.Log(coin);
+            bubbles++;
+            bubbleText.text = "Bubbles: " + bubbles.ToString();
+            Debug.Log(bubbles);
             Destroy(other.gameObject);
         }
     }
