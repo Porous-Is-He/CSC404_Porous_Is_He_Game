da6b23ce5a46c91d72e565052c4f13dacf9b971f
chenj605 <jianjia.chen@mail.utoronto.ca>
2023-12-04 08:12:32 -0500

    lvl 2, anims

---
+++ Porous Is He/Assets/Lander.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lander : MonoBehaviour
+{
+    private List<GameObject> ObjectsEntered = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public bool IsGrounded()
+    {
+        return ObjectsEntered.Count > 0;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (!other.isTrigger)
+        {
+            ObjectsEntered.Remove(other.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.isTrigger)
+        {
+            Debug.Log(other.gameObject.name);
+            ObjectsEntered.Add(other.gameObject);
+        }
+    }
+}
